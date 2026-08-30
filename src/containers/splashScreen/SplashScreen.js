5d1e8e6a4ce214fbba5a359123c37fda60c3d523
Lum Ko Sand <42332579+dnasok@users.noreply.github.com>
2026-08-30 19:00:28 +0800

    feat: update splash logo and header alignment

--- src/containers/splashScreen/SplashScreen.js
+++ src/containers/splashScreen/SplashScreen.js
@@ -1,15 +1,19 @@
 import React, {useContext} from "react";
 import "./SplashScreen.css";
-import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import {greeting, splashScreen} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function SplashScreen() {
   const {isDark} = useContext(StyleContext);
+
   return (
     <div className={isDark ? "dark-mode splash-container" : "splash-container"}>
       <div className="splash-animation-container">
-        <DisplayLottie animationData={splashScreen.animation} />
+        <img
+          src={splashScreen.animation}
+          alt="Personal logo"
+          className="splash-logo"
+        />
       </div>
       <div className="splash-title-container">
         <span className="grey-color"> &lt;</span>
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,15 +1,15 @@
-﻿/* Change this file to get your personal Portfolio */
+/* Change this file to get your personal Portfolio */
 
 // To change portfolio colors globally go to the  _globalColor.scss file
 
 import emoji from "react-easy-emoji";
-import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+import personalLogo from "./assets/images/personal_logo.png";
 
 // Splash Screen
 
 const splashScreen = {
   enabled: true, // set false to disable splash screen
-  animation: splashAnimation,
+  animation: personalLogo,
   duration: 2000 // Set animation duration as per your animation
 };
 
